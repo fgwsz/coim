feb41bf600748f4377c4e838595287047b116cb9
fgwsz <3191000458@qq.com>
2024-12-13 23:12:30 +0800

    clean

--- source/coim/IsEnum.hpp
+++ source/coim/IsEnum.hpp
@@ -17,7 +17,8 @@ requires
     &&(!::coim::is_pointer_v<Type_>)
     &&(!::coim::is_member_pointer_v<Type_>)
     &&(!::coim::is_fundamental_v<Type_>)
-    //isn't union or struct or class
+    //isn't union or class
+    //原因是C++内存布局的union和class会在编译期间因为强制类型转换失败而不满足约束
     &&(requires(Type_ value_){reinterpret_cast<Type_>(value_);})
 struct IsEnum<Type_>: ::coim::true_type{};
 }
--- source/coim/IsIntegral.hpp
+++ source/coim/IsIntegral.hpp
@@ -6,6 +6,7 @@ struct IsIntegral: ::coim::bool_constant<
     requires(Type_ value_,Type_* pointer_,void(*function_)(Type_)){
         //Type_*=>Type_ isn't reference
         reinterpret_cast<Type_>(value_);//Type_ isn't class
+        //原因是C++内存布局的union和class会在编译期间因为强制类型转换失败而不满足约束
         function_(0);//Type_ isn't enum
         pointer_+value_;//Types_ isn't floating point
     }
